50a7036ff42e80a79248a7e013f78969731c1c11
Pham Hoang Vu <phamvuhoang486@gmail.com>
2026-05-21 14:34:13 +0700

    feat(DS-23-01): implement filter jobs endpoint

--- backend/src/core/api/index.js
+++ backend/src/core/api/index.js
@@ -15,6 +15,6 @@ export const ModuleResolver = HandlerResolver
         UserResolver,
         MediaResolver,
         ProfileResolver,
-        ApplicationsResolver
+        ApplicationsResolver,
         JobResolver,
     ]);
